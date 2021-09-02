16f245fc9f224a57af5f2b89e5b1dea5dd5f35ac
Safin-Ahmed <safin.ahmed2000@gmail.com>
2021-09-02 14:24:19 +0600

    everything is done except error handling and search result tracking

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
-fetch('https://openlibrary.org/search.json?q=javascript')
-.then(res => res.json())
-.then(data => console.log(data));
+// Get Search Field Value 
+const getSearchText = () => {
+    const searchField = document.getElementById('search-field');
+    const searchFieldText = searchField.value;
+    searchField.value = '';
+    loadData(searchFieldText);
+}
+
+// Load Data According To Search Value
+const loadData = async data => {
+   const getResponse = await fetch(`https://openlibrary.org/search.json?q=${data}`);
+   const convertResponse = await getResponse.json();
+   displayData(convertResponse);
+}
+
+// Display The Result in DOM 
+
+const displayData = data => {
+    console.log(data);
+    const searchResults = document.getElementById('search-results');
+    searchResults.textContent = '';
+    data.docs.forEach(element => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card shadow">
+                <img src="https://covers.openlibrary.org/b/id/${element.cover_i ? element.cover_i : '404'}-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h4 class="card-title">${element.title.slice(0, 50)}</h4>
+                    <h6 class="card-title">Author Name: ${element.author_name ? element.author_name[0] : ''}</h6>
+                    <h6 class="card-title">Publisher: ${element.publisher ? element.publisher[0] : ''}</h6>
+                    <h6 class="card-title">First Published: ${element.publish_date ? element.publish_date[0] : ''}
+                    </h6>
+                </div>
+            </div>
+        `;
+        searchResults.appendChild(div);
+    });
+}
